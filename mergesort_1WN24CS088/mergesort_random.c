b7ae1309d55286e2cbe97bb43de84fdd8aa1dcf3
deepthigirish18 <deepthigirish.cs24@bmsce.ac.in>
2026-03-13 12:45:09 +0530

    Add files via upload

---
+++ mergesort_1WN24CS088/mergesort_random.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+    for(i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for(j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    i = 0;
+    j = 0;
+    k = l;
+    while(i < n1 && j < n2) {
+        if(L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while(j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }}
+void mergeSort(int arr[], int l, int r) {
+    if(l < r) {
+        int m = (l + r) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+int main() {
+    int n, i;
+    clock_t start, end;
+    double cpu_time;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    srand(time(0));
+    printf("\nGenerated random numbers:\n");
+    for(i = 0; i < n; i++) {
+        arr[i] = rand() % 100;
+        printf("%d ", arr[i]);
+    }
+    start = clock();
+    mergeSort(arr, 0, n - 1);
+    end = clock();
+    cpu_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("\n\nSorted array:\n");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n\nTime taken = %f seconds\n", cpu_time);
+    return 0;
+}
